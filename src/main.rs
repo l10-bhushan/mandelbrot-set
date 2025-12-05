c743affbb3f74208d48b8c441c0393731ccd571d
Bhushan <bhushan.rajput.l1odevs@gmail.com>
2025-12-05 20:08:28 +0530

    Final commit for mandelbrot visualization

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,78 @@
+#![allow(warnings)]
 use num::complex::Complex;
-const WIDTH: usize = 1000;
-const HEIGHT: usize = 24;
-const MAX_ITERATIONS: u16 = 1000;
+use std::collections::HashMap;
 
-#[derive(Debug)]
-struct Canvas {
-    view_port: Vec<Vec<f64>>,
+const WIDTH: i16 = 100;
+const HEIGHT: i8 = 24;
+const X_START: f64 = -2.0;
+const X_END: f64 = 2.0;
+const Y_END: f64 = -1.0;
+const Y_START: f64 = 1.0;
+const MAX_ITERS: isize = 1000;
+
+fn create_canvas() -> Vec<Vec<(f64, f64)>> {
+    let mut axis: Vec<_> = Vec::new();
+    for y in 0..HEIGHT {
+        let mut row = Vec::new();
+        for x in 0..WIDTH {
+            let x_axis = X_START + (x as f64 * (X_END - X_START)) / (WIDTH - 1) as f64;
+            let y_axis = Y_START + (y as f64 * (Y_END - Y_START)) / (HEIGHT - 1) as f64;
+            row.push((x_axis, y_axis));
+        }
+        axis.push(row);
+    }
+    axis
 }
 
-impl Canvas {
-    fn new() -> Canvas {
-        Canvas {
-            view_port: vec![vec![0.0; WIDTH]; HEIGHT],
+fn escaped_at(co_ordinates: (f64, f64)) -> isize {
+    let mut z = Complex { re: 0.00, im: 0.00 };
+    let c = Complex {
+        re: co_ordinates.0,
+        im: co_ordinates.1,
+    };
+    for i in 0..=MAX_ITERS {
+        if z.norm() > 2.0 {
+            return i;
         }
+        z = (z * z) + c;
     }
+    MAX_ITERS
 }
 
-fn create_axis_values(canvas: &mut Canvas) {
-    let mid_x = (WIDTH / 2) as f64;
-    let mid_y = (HEIGHT / 2) as f64;
-    let step_size_x: f64 = (mid_x - (-mid_x)) / (WIDTH - 1) as f64;
-    let step_size_y: f64 = (mid_y - (-mid_y)) / (HEIGHT - 1) as f64;
-    println!("mid and step size : {mid_x} {mid_y} {step_size_x} {step_size_y} ");
-    for i in 1..HEIGHT {
-        for j in 1..WIDTH {
-            let x_axis_val: f64 = (-mid_x + (j - 1) as f64 * step_size_x).round();
-            let y_axis_val: f64 = (-mid_y + (i - 1) as f64 * step_size_y).round();
-            let tuple = (x_axis_val, y_axis_val);
-            let iters = distance(tuple);
-            canvas.view_port[i - 1][j - 1] = iters;
-        }
+fn plot_manderblot(escaped_at_vec: Vec<isize>) {
+    let mut str = String::new();
+    for item in escaped_at_vec {
+        let val = match item {
+            0..=2 => ' ',
+            0..=5 => '.',
+            5..=10 => '•',
+            11..=30 => '*',
+            30..=100 => '+',
+            100..=200 => 'x',
+            200..=400 => '$',
+            400..700 => '#',
+            _ => '%',
+        };
+        str.push(val);
     }
-}
-
-fn distance(co_ordinates: (f64, f64)) -> f64 {
-    let mut z = Complex { re: 0.0, im: 0.0 };
-    let complex_number = Complex::new(co_ordinates.0, co_ordinates.1);
-    for i in 0..=MAX_ITERATIONS {
-        if z.norm() == 2.0 {
-            return i as f64;
-        }
-        z = z * z + complex_number;
-    }
-    MAX_ITERATIONS as f64
+    println!("{}", str);
 }
 
 fn main() {
-    println!("---- Mandelbrot plot ----");
-
-    let mut canvas = Canvas::new();
-    create_axis_values(&mut canvas);
-    println!("{:?}", canvas.view_port)
+    println!("MandelBrot Set visualization in Rust");
+    let axis = create_canvas();
+    let mut iter_vec = Vec::new();
+    for item in axis {
+        for row_item in item {
+            let iter = escaped_at(row_item);
+            iter_vec.push(iter);
+        }
+    }
+    println!("Escapted at vec : {:?}", iter_vec);
+    let mut map = HashMap::new();
+    for val in &iter_vec {
+        *map.entry(val).or_insert(0) += 1;
+    }
+    println!("{:?}", map);
+    plot_manderblot(iter_vec);
 }
